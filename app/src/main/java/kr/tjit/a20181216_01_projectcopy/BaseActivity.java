91d6dcd62763ed3a6376981e490fd5762843d50f
mmacarons <manymany.macarons@gmail.com>
2018-12-20 17:24:25 +0900

    메뉴 선택 팝업

---
+++ app/src/main/java/kr/tjit/a20181216_01_projectcopy/BaseActivity.java
@@ -0,0 +1,16 @@
+package kr.tjit.a20181216_01_projectcopy;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+
+}
+
+
--- app/src/main/java/kr/tjit/a20181216_01_projectcopy/MainActivity.java
+++ app/src/main/java/kr/tjit/a20181216_01_projectcopy/MainActivity.java
@@ -2,12 +2,109 @@ package kr.tjit.a20181216_01_projectcopy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    LinearLayout zzimTangBtn;
+    LinearLayout nightFoodBtn;
+    LinearLayout jokbalBtn;
+    LinearLayout ChineseFoodBtn;
+    LinearLayout pizzaBtn;
+    LinearLayout chickenBtn;
+    LinearLayout japanFoodBtn;
+    LinearLayout bunsikBtn;
+    LinearLayout koreanFoodBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        setValues();
+        setupEvents();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+        zzimTangBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "찜.탕을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        nightFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "야식을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        jokbalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "족발.보쌈을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ChineseFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "중국집을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        pizzaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "피자를 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        chickenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "치킨을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        japanFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "돈까스.회.일식을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        bunsikBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "분식을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        koreanFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "한식을 선택했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+        zzimTangBtn = findViewById(R.id.zzimTangBtn);
+        nightFoodBtn = findViewById(R.id.nightFoodBtn);
+        jokbalBtn = findViewById(R.id.jokbalBtn);
+        ChineseFoodBtn = findViewById(R.id.ChineseFoodBtn);
+        pizzaBtn = findViewById(R.id.pizzaBtn);
+        chickenBtn = findViewById(R.id.chickenBtn);
+        japanFoodBtn = findViewById(R.id.japanFoodBtn);
+        bunsikBtn = findViewById(R.id.bunsikBtn);
+        koreanFoodBtn = findViewById(R.id.koreanFoodBtn);
+
     }
 }
