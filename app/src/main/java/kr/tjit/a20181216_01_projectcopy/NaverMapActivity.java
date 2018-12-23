f96c66b60d3361aa561da5c0997ea70218d8c7b6
manymany.macarons <manymany.macarons@gmail.com>
2018-12-23 15:42:41 +0900

    네이버 지도 화면 추가

---
+++ app/src/main/java/kr/tjit/a20181216_01_projectcopy/NaverMapActivity.java
@@ -0,0 +1,27 @@
+package kr.tjit.a20181216_01_projectcopy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
+
+public class NaverMapActivity extends NMapActivity {
+
+    private NMapView mMapView;
+    private String CLIENT_ID = "임시로 작성";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMapView = new NMapView(NaverMapActivity.this);
+        setContentView(mMapView);
+        mMapView.setClientId(CLIENT_ID);
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
+
+    }
+}
