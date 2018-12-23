8e7d4603bbb06354379c97cb8e9d2c1de96451b1
manymany.macarons <manymany.macarons@gmail.com>
2018-12-23 11:17:01 +0900

    온클릭리스너를 변수에 담아서 이벤트 일괄 처리

--- app/src/main/java/kr/tjit/a20181216_01_projectcopy/MainActivity.java
+++ app/src/main/java/kr/tjit/a20181216_01_projectcopy/MainActivity.java
@@ -51,30 +51,61 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
         //한식 버튼이 눌렸을 때 할 일
-        koreanFoodBtn.setOnClickListener(new View.OnClickListener() {
+/*        koreanFoodBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(mContext, "한식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
 
+//                한식이 눌렸다는 사실을 레스토랑리스트 액티비티에 공지. (알려줌)
+
 //                ctrl+R 사용해서 전부 치환 가능
                 Intent intent = new Intent(mContext, RestaurantListActivity.class);
+                intent.putExtra("음식종류", "한식");
                 startActivity(intent);          //티켓을 발급받아서 출발함
             }
-        });
+        });*/
 
-        bunsikBtn.setOnClickListener(new View.OnClickListener() {
+
+//        OnClickListener도 변수에 넣을 수 있음, 자료형은 OnClickListener
+//        여기서 변수명은 foodKindClickListener
+        View.OnClickListener foodKindClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mContext, "분식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        japaneseFoodBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(mContext, "돈까스.회.일식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+//            첫번째 방법: 원초적인 방법임. v가 가진 id를 비교해서 각각의 이벤트를 적용
+//            별로 효율적이지 못 함.
+/*
+                if (v.getId() == R.id.chickenBtn) {
+                    Toast.makeText(mContext, "치킨 눌림", Toast.LENGTH_SHORT).show();
+                }
+                else if (v.getId() == R.id.pizzaBtn) {
+                    Toast.makeText(mContext, "피자 눌림", Toast.LENGTH_SHORT).show();
+                }
+*/
+
+//            두번째 방법: xml에 태그를 달아서 메모처럼 활용
+//            view는 각각의 리니어레이아웃을 의미
+                Toast.makeText(mContext, v.getTag().toString(), Toast.LENGTH_SHORT).show();
+
+
             }
-        });
+
+
+
+        };
+
+//        모든 버튼이 위의 이벤트 처리를 공유하도록 함.
+//        setOnClickListener에 new OnClickListener 대신 위에서 만든 변수를 넣어줌.
+        koreanFoodBtn.setOnClickListener(foodKindClickListener);
+        bunsikBtn.setOnClickListener(foodKindClickListener);
+        japaneseFoodBtn.setOnClickListener(foodKindClickListener);
+        chickenBtn.setOnClickListener(foodKindClickListener);
+        pizzaBtn.setOnClickListener(foodKindClickListener);
+        chineseBtn.setOnClickListener(foodKindClickListener);
+        jokbalBtn.setOnClickListener(foodKindClickListener);
+        yasikBtn.setOnClickListener(foodKindClickListener);
+        stewBtn.setOnClickListener(foodKindClickListener);
+
 
     }
 
